ab111239edfaa41250377ce144dcdb79ad2f20c5
briiiqtt <briiiqtt@gmail.com>
2022-01-10 20:15:05 +0900

    자재발주 전체리스트 완 모달관련 풀 목적 커밋

--- solar/src/main/java/solar/rsc/cmmn/web/RscController.java
+++ solar/src/main/java/solar/rsc/cmmn/web/RscController.java
@@ -3,10 +3,11 @@ package solar.rsc.cmmn.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import solar.rsc.ordr.service.RscOrdrService;
-import solar.rsc.ordr.service.impl.RscOrdrMapper;
 
 @Controller
 public class RscController {
@@ -14,9 +15,22 @@ public class RscController {
 	@Autowired
 	RscOrdrService rscOrdrService;
 	
-	@RequestMapping("rsc/ordr")
-	public String rscOrdr(Model model) {
-		model.addAttribute("rscOrdr",rscOrdrService.selectAll()); 
+	@GetMapping("rsc/ordr")
+	public String rscOrdr() {
 		return "rsc/ordr";
+	}
+	
+	@GetMapping("rsc/ordrData")	
+	public String rscOrdrData(@RequestParam(value = "ordrDtStt",required = false) String ordrDtStt,
+							  @RequestParam(value = "ordrDtEnd",required = false) String ordrDtEnd,
+							  @RequestParam(value = "co",required = false) String co,
+							  @RequestParam(value = "rsc",required = false) String rsc,
+							  Model model) {
+		System.out.println(ordrDtStt);
+		System.out.println(ordrDtEnd);
+		System.out.println(co);
+		System.out.println(rsc);
+		model.addAttribute("rscOrdr",rscOrdrService.selectAll());
+		return "jsonView";
 	}
 }
--- solar/src/main/java/solar/rsc/ordr/service/RscOrdr.java
+++ solar/src/main/java/solar/rsc/ordr/service/RscOrdr.java
@@ -1,33 +1,27 @@
 package solar.rsc.ordr.service;
 
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import lombok.Data;
   
 @Data
 public class RscOrdr {
-	String ordrCd;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	Date ordrDt;
+	String ordrDt;
+	String rscNm;
 	String rscCd;
 	String ordrQty;
-	String coCd;
+	String ordrCd;
+	String coNm;
 	String rscIstQty;
-	String rscInferQty;
-	String inspCls;
-	public String getOrdrCd() {
-		return ordrCd;
-	}
-	public void setOrdrCd(String ordrCd) {
-		this.ordrCd = ordrCd;
-	}
-	public Date getOrdrDt() {
+	public String getOrdrDt() {
 		return ordrDt;
 	}
-	public void setOrdrDt(Date ordrDt) {
+	public void setOrdrDt(String ordrDt) {
 		this.ordrDt = ordrDt;
+	}
+	public String getRscNm() {
+		return rscNm;
+	}
+	public void setRscNm(String rscNm) {
+		this.rscNm = rscNm;
 	}
 	public String getRscCd() {
 		return rscCd;
@@ -41,11 +35,17 @@ public class RscOrdr {
 	public void setOrdrQty(String ordrQty) {
 		this.ordrQty = ordrQty;
 	}
-	public String getCoCd() {
-		return coCd;
+	public String getOrdrCd() {
+		return ordrCd;
 	}
-	public void setCoCd(String coCd) {
-		this.coCd = coCd;
+	public void setOrdrCd(String ordrCd) {
+		this.ordrCd = ordrCd;
+	}
+	public String getCoNm() {
+		return coNm;
+	}
+	public void setCoNm(String coNm) {
+		this.coNm = coNm;
 	}
 	public String getRscIstQty() {
 		return rscIstQty;
@@ -53,24 +53,10 @@ public class RscOrdr {
 	public void setRscIstQty(String rscIstQty) {
 		this.rscIstQty = rscIstQty;
 	}
-	public String getRscInferQty() {
-		return rscInferQty;
-	}
-	public void setRscInferQty(String rscInferQty) {
-		this.rscInferQty = rscInferQty;
-	}
-	public String getInspCls() {
-		return inspCls;
-	}
-	public void setInspCls(String inspCls) {
-		this.inspCls = inspCls;
-	}
 	@Override
 	public String toString() {
-		return "RscOrdr [ordrCd=" + ordrCd + ", ordrDt=" + ordrDt + ", rscCd=" + rscCd + ", ordrQty=" + ordrQty
-				+ ", coCd=" + coCd + ", rscIstQty=" + rscIstQty + ", rscInferQty=" + rscInferQty + ", inspCls="
-				+ inspCls + "]";
+		return "RscOrdr [ordrDt=" + ordrDt + ", rscNm=" + rscNm + ", rscCd=" + rscCd + ", ordrQty=" + ordrQty
+				+ ", ordrCd=" + ordrCd + ", coNm=" + coNm + ", rscIstQty=" + rscIstQty + "]";
 	}
-	
 	
 }
