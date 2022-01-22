e36c7c8632757c1cb145b4d9c23ec7fb49bcb847
briiiqtt <briiiqtt@gmail.com>
2022-01-22 15:54:58 +0900

    불량률+불량량 일단 완성

--- solar/src/main/java/solar/rsc/infer/service/RscInferRate.java
+++ solar/src/main/java/solar/rsc/infer/service/RscInferRate.java
@@ -4,8 +4,18 @@ import lombok.Data;
 
 @Data
 public class RscInferRate {
-	String inferRate;
-	String inferQty;
-	String ordrQty;
+	String inferRateFirst;
+	String inferQtyFirst;
+	String ordrQtyFirst;
+	String inferRateSecond;
+	String inferQtySecond;
+	String ordrQtySecond;
+	String inferRateThird;
+	String inferQtyThird;
+	String ordrQtyThird;
+	String inferRateFourth;
+	String inferQtyFourth;
+	String ordrQtyFourth;
 	String coCd;
+	String coNm;
 }
