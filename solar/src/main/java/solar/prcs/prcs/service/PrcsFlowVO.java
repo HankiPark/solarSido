7d3dd4047187bfaf4b57d86da0fe760c54e2b753
gumzzzi <shypo0@naver.com>
2022-02-04 18:12:58 +0900

    0204

---
+++ solar/src/main/java/solar/prcs/prcs/service/PrcsFlowVO.java
@@ -0,0 +1,15 @@
+package solar.prcs.prcs.service;
+
+import lombok.Data;
+
+@Data
+public class PrcsFlowVO {
+
+	String prcsOrd;
+	String prdtCd;
+	String eqmCd;
+	String prcsCd;
+	String prcsNm;
+	String prcsDesct;
+	
+}
