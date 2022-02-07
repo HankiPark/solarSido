ead9d052ccd441d864a60ac045f55a0a7c051346
gumzzzi <shypo0@naver.com>
2022-02-07 17:52:40 +0900

    0207 공정최종완료제품 재고테이블등록 구현

--- solar/src/main/java/solar/prcs/prcs/service/impl/PrcsMapper.java
+++ solar/src/main/java/solar/prcs/prcs/service/impl/PrcsMapper.java
@@ -7,6 +7,7 @@ import solar.prcs.prcs.service.IndicaVO;
 import solar.prcs.prcs.service.PrcsEqmVO;
 import solar.prcs.prcs.service.PrcsFlowVO;
 import solar.prcs.prcs.service.PrcsPrMVO;
+import solar.prcs.prcs.service.PrdtStcVO;
 import solar.prcs.prcs.service.RscConVO;
 import solar.prcs.prcs.service.RsltVO;
 
@@ -19,6 +20,7 @@ public interface PrcsMapper {
 	List<ClotVO> selectPrcsItem(ClotVO vo);
 	int updateRscClot(ClotVO vo);
 	int insertRscClot(ClotVO vo);
+	int insertPrdtStc(PrdtStcVO vo);
 	
 	List<PrcsEqmVO> selectPrcs(PrcsEqmVO vo);
 	List<PrcsFlowVO> selectPrcsFlow(PrcsFlowVO vo);
